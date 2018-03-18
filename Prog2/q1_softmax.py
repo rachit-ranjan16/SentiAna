67de1beab84e83d0ab0da20d80923b4cd2edfbb1
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-03-18 15:51:14 -0400

    Initial Commit Starter Code Vagrant Scripts

---
+++ Prog2/q1_softmax.py
@@ -0,0 +1,87 @@
+import numpy as np
+import random
+
+def softmax(x):
+    """
+    Compute the softmax function for each row of the input x.
+
+    It is crucial that this function is optimized for speed because
+    it will be used frequently in later code.
+    You might find numpy functions np.exp, np.sum, np.reshape,
+    np.max, and numpy broadcasting useful for this task. (numpy
+    broadcasting documentation:
+    http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html)
+
+    You should also make sure that your code works for one
+    dimensional inputs (treat the vector as a row), you might find
+    it helpful for your later problems.
+
+    You must implement the optimization in problem 1(a) of the
+    written assignment!
+    """
+
+    ### YOUR CODE HERE
+    # if len(x.shape) > 1:
+    #     # For higher dimension arrays
+    #     max_x = np.max(x, axis = 1)
+    #     x -= max_x.reshape((x.shape[0], 1))
+    #     x = np.exp(x)
+    #     max_x = np.sum(x, axis = 1)
+    #     x /= max_x.reshape((x.shape[0], 1))
+    # else:
+    #     # For single dimension arrays
+    #     max_x = np.max(x)
+    #     x -= max_x
+    #     x = np.exp(x)
+    #     max_x = np.sum(x)
+    #     x /= max_x
+
+
+    orig_shape = x.shape
+    c = np.max(x, axis=-1, keepdims=True)
+    exps = np.exp(x - c)
+    sum_exps = np.sum(exps, axis=-1, keepdims=True)
+    x = exps / sum_exps
+    return x
+    ### END YOUR CODE
+
+
+
+def test_softmax_basic():
+    """
+    Some simple tests to get you started.
+    Warning: these are not exhaustive.
+    """
+    print ("Running basic tests...")
+    test1 = softmax(np.array([1,2]))
+    print (test1)
+    assert np.amax(np.fabs(test1 - np.array(
+        [0.26894142,  0.73105858]))) <= 1e-6
+
+    test2 = softmax(np.array([[1001,1002],[3,4]]))
+    print ("Printing test2 Output\n%r" % test2)
+    assert np.amax(np.fabs(test2 - np.array(
+        [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
+
+    test3 = softmax(np.array([[-1001,-1002]]))
+    print (test3)
+    assert np.amax(np.fabs(test3 - np.array(
+        [0.73105858, 0.26894142]))) <= 1e-6
+
+    print ("You should verify these results!\n")
+
+def test_softmax():
+    """
+    Use this space to test your softmax implementation by running:
+        python q1_softmax.py
+    This function will not be called by the autograder, nor will
+    your tests be graded.
+    """
+    print ("Running your tests...")
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+if __name__ == "__main__":
+    test_softmax_basic()
+    test_softmax()
---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+echo "----------------------------"
+echo "Updating"
+echo "----------------------------"
+sudo apt-get -y -q update
+# Uncomment for Upgrading - Takes Longer
+#sudo apt-get upgrade
+echo "----------------------------"
+echo "Install and Configture vim"
+echo "----------------------------"
+sudo apt-get -y -q install vim
+sudo cp /vagrant_data/.vimrc /home/ubuntu
+sudo cp /vagrant_data/.vimrc /root/
+echo "----------------------------"
+echo "Install pip"
+echo "----------------------------"
+sudo apt-get -y -q install python3-pip
+echo "----------------------------"
+echo "Install pyenv"
+echo "----------------------------"
+sudo apt-get -y -q install python3-venv
+echo "----------------------------"
+echo "Create and Activate Virtual Environment"
+echo "----------------------------"
+pyvenv developEnv
+source developEnv/bin/activate
+pip install --upgrade pip
+echo "----------------------------"
+echo "Install Specified Dependencies in VirtualEnv"
+echo "----------------------------"
+pip install -r /Prog2/requirements.txt
+echo "----------------------------"
+echo "Install and configure git"
+echo "----------------------------"
+sudo apt-get -y -q install git
+git config --global user.name "rachit.ranjan16"
+git config --global user.email "rachit.ranjan93@gmail.com"
